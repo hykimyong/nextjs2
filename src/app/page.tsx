cc7ab838b22f8ed81fcb4d477dc67a6c438c4726
hykimyong <hykimyong@naver.com>
2023-04-26 22:44:39 +0900

    리액트 기본문법

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 export default function Home() {
+  const name = 'park';
+  const link = 'http://google.com';
   return (
     <div>
-      </div>
+      <h4 className="title">애플후레시</h4>
+      <p className="title-sub">by dev {name}</p>
+      <a href={link}></a>
+    </div>
   );
 }
