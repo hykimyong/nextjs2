25a450360a14c44aeb9aa22a8e77228385b0e333
hykimyong <hykimyong@naver.com>
2023-04-29 01:08:21 +0900

    next image사용하기
    최적화에 좋음

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,14 +1,19 @@
+import Image from "next/image";
+
 export default function Home() {
+    const 상품: string[] = ['Tomatoes','Pasta','Coconut'];
     
     return (
       <div>
         <h4 className="title">상품목록</h4>
-        <div className="food">
-            <h4>상품1 $40</h4>
-        </div>
-        <div className="food">
-            <h4>상품2 $40</h4>
-        </div>
+        {
+          상품.map((product,index) => (
+            <div className="food" key={index}>
+              <Image className="food-img" src={`/food${index}.png`} alt="토마토" width={500} height={500}/>
+              <h4>{product} $40</h4>
+          </div>
+          ))
+        }
       </div>
     );
   }
