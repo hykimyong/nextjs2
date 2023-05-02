81da45e5b3607f817915271d815e6bd550c13318
hykimyong <hykimyong@naver.com>
2023-05-02 22:31:31 +0900

    props로 전달하기

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,23 +1,31 @@
 import data from'./data';
 
 export default function Cart() {
+  const 장바구니 = ['Tomatoes','Pasta'];
     return (
       <div>
         <h4 className="title">Cart</h4>
-        <div className="cart-item">
-          <p>상품명1 {data.name}</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
-        <CartItem/>
+        <CartItem 작명={장바구니[0]}/>
+        <CartItem 작명={장바구니[1]}/>
+        <Banner content="롯데카드"/>
+        <Banner content="현대카드"/>
       </div>
     )
 }
+interface BannerNameProps{
+  content: string;
+}
+function Banner(props:BannerNameProps) {
+  return <h5>{props.content} 결제 행사중</h5>
+}
 
-function CartItem(){
+interface CartItemProps {
+  작명: string;
+}
+function CartItem(props:CartItemProps){
     return(
         <div className="cart-item">
-          <p>상품명</p>
+          <p>상품명 {props.작명}</p>
           <p>$40</p>
           <p>1개</p>
         </div>
