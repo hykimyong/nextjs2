4c1141c240b7d377587cc277ffd878c890969efd
hykimyong <hykimyong@naver.com>
2023-05-08 22:08:42 +0900

    주석제거

--- src/util/database.ts
+++ src/util/database.ts
@@ -2,20 +2,20 @@ import * as mongoDB from "mongodb";
 import * as dotenv from "dotenv";
 
 export async function connectToDatabase () {
-  //   dotenv.config();
+    dotenv.config();
  
   //   const client: mongoDB.MongoClient = new mongoDB.MongoClient('mongodb+srv://admin:1234@cluster0.ip0hlgj.mongodb.net/?retryWrites=true&w=majority');
   const client: mongoDB.MongoClient = new mongoDB.MongoClient('');
             
-  //   await client.connect();
+    await client.connect();
         
-  //   const db: mongoDB.Db = client.db('forum');
+    const db: mongoDB.Db = client.db('forum');
    
-  //   const gamesCollection: mongoDB.Collection = db.collection('post');
+    const gamesCollection: mongoDB.Collection = db.collection('post');
  
-  // collections.games = gamesCollection;
+  collections.games = gamesCollection;
        
-  //        console.log(`Successfully connected to database: ${db.databaseName} and collection: ${gamesCollection.collectionName}`);
+         console.log(`Successfully connected to database: ${db.databaseName} and collection: ${gamesCollection.collectionName}`);
  }
 
 export const collections: { games?: mongoDB.Collection } = {}
